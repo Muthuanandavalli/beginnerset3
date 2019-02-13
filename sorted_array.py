bc1a23262105a22b2752ff037915929e8165e023
Muthuanandavalli <47178001+Muthuanandavalli@users.noreply.github.com>
2019-02-13 23:08:50 +0530

    Update sorted_array.py

--- sorted_array.py
+++ sorted_array.py
@@ -1,8 +1,8 @@
 N=int(input())
 a=list(map(int,input().split()))
-y=sorted(a)
-for c in range(0,len(y)):
-	if c<(len(y)-1):
-		print(y[c],end=" ")
+x=sorted(a)
+for c in range(0,len(x)):
+	if c<(len(x)-1):
+		print(x[c],end=" ")
 	else:
-		print(y[c])
+		print(x[c])
